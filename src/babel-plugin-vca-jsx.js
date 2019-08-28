83d29584b2a95e2f40f0be03b1a32894c6dc273e
luffy <luffy@asus18.04.com>
2019-08-28 15:47:58 +0800

    fix: Support more situations

--- src/babel-plugin-vca-jsx.js
+++ src/babel-plugin-vca-jsx.js
@@ -1,5 +1,7 @@
 const syntaxJsx = require('@babel/plugin-syntax-jsx').default;
 
+const importSource = '@vue/composition-api';
+
 const hasJSX = (t, path) => {
     const JSXChecker = {
         hasJSX: false
@@ -12,18 +14,46 @@ const hasJSX = (t, path) => {
     return JSXChecker.hasJSX;
 };
 
-const hasSetup = (t, path) => {
-    const SetupChecker = {
-        hasSetup: false
-    };
+// remove `var h = this.$createElement;` in `setup()`
+const remove$createElement = (t, path) => {
     path.traverse({
         ObjectMethod(p) {
-            if (p.node.key.name === 'setup') {
-                this.hasSetup = true;
+            const isSetup = p.node.key.name === 'setup';
+            if (!isSetup) return;
+            p.traverse({
+                VariableDeclaration(varPath) {
+                    varPath.traverse({
+                        MemberExpression(p) {
+                            if (t.isThisExpression(p.node.object) && t.isIdentifier(p.node.property) && p.node.property.name === '$createElement') {
+                                varPath.remove();
+                            }
+                        }
+                    })
+                }
+            });
+        }
+    });
+};
+
+// auto import `createElement as h` from `@vue/composition-api`
+const autoImport = (t, path) => {
+    if (hasJSX(t, path)) {
+        const importNodes = path.get('body').filter(p => p.isImportDeclaration()).map(p => p.node);
+        const vfaImportNodes = importNodes.filter(p => p.source.value === importSource);
+        const hasH = vfaImportNodes.some(p => (
+            p.specifiers.some(s => (
+                t.isImportSpecifier(s) && s.imported.name === 'createElement' && s.local.name === 'h'
+            ))
+        ));
+        if (!hasH) {
+            const vfaImportSpecifier = t.importSpecifier(t.identifier('h'), t.identifier('createElement'));
+            if (vfaImportNodes.length > 0) {
+                vfaImportNodes[0].specifiers.push(vfaImportSpecifier);
+            } else {
+                path.unshiftContainer('body', t.importDeclaration([vfaImportSpecifier], t.stringLiteral(importSource)));
             }
         }
-    }, SetupChecker);
-    return SetupChecker.hasSetup;
+    }
 }
 
 module.exports = ({ types: t }) => {
@@ -31,41 +61,8 @@ module.exports = ({ types: t }) => {
         inherits: syntaxJsx,
         visitor: {
             Program(path) {
-                path.traverse({
-                    ObjectMethod(p) {
-                        if (!hasJSX(t, p) || !this.hasSetup) return;
-
-                        p.traverse({
-                            VariableDeclaration(varPath) {
-                                varPath.traverse({
-                                    MemberExpression(p) {
-                                        if (t.isThisExpression(p.node.object) && t.isIdentifier(p.node.property) && p.node.property.name === '$createElement') {
-                                            varPath.remove();
-                                        }
-                                    }
-                                })
-                            }
-                        });
-
-                        const importNodes = path.get('body').filter(p => p.isImportDeclaration()).map(p => p.node);
-                        const vfaImportNodes = importNodes.filter(p => p.source.value === '@vue/composition-api');
-                        const hasH = vfaImportNodes.some(p => (
-                            p.specifiers.some(s => (
-                                t.isImportSpecifier(s) && s.imported.name === 'createElement' && s.local.name === 'h'
-                            ))
-                        ));
-                        if (!hasH) {
-                            const vfaImportSpecifier = t.importSpecifier(t.identifier('h'), t.identifier('createElement'));
-                            if (vfaImportNodes.length > 0) {
-                                vfaImportNodes[0].specifiers.push(vfaImportSpecifier);
-                            } else {
-                                path.unshiftContainer('body', t.importDeclaration([vfaImportSpecifier], t.stringLiteral('@vue/composition-api')));
-                            }
-                        }
-                    }
-                }, {
-                    hasSetup: hasSetup(t, path)
-                });
+                remove$createElement(t, path);
+                autoImport(t, path);
             }
         }
     }
