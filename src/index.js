a88df053889c3b93ddf0af756430d7b46a6599ed
luwanquan <1501474302@qq.com>
2019-12-13 00:26:41 +0800

    fixed babel-preset-jsx babel-sugar-v-model v1.1.2 calling this in setup()

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ module.exports = function() {
         plugins: [
             require('./babel-sugar-inject-h'),
             require('./babel-sugar-setup-functional'),
-            require('./babel-sugar-setup-ref')
+            require('./babel-sugar-setup-ref'),
+            require('./babel-sugar-v-model-v1.1.2-patch')
         ]
     };
 };
